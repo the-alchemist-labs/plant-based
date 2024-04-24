fd2789f2538be876cde04ec4a28b035458a5b1fb
Axther <mailtoarthur@gmail.com>
2024-04-24 15:58:07 +0300

    Fixing file names

--- Assets/Scripts/Background Scroll.cs
+++ Assets/Scripts/Background Scroll.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-
+    private Vector3 StartPos; // The starting position of the background
+    private float repeatWidth;
     public float speed; //The speed the background will scroll
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartPos = transform.position; // the start position of the background
+        repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     // Update is called once per frame
@@ -20,5 +22,10 @@ public class MoveLeft : MonoBehaviour
             transform.Translate(Vector3.left * Time.deltaTime * speed); //Some unity thingy that makes the background move
         }
         else transform.Translate(Vector3.left * Time.deltaTime); //incase it's under 1 or negetive, the default will be 1
+
+        if (transform.position.x < StartPos.x - repeatWidth)
+        {
+            transform.position = StartPos; //snap back to starting position, so scroll starts over
+        }
     }
 }
